8768b4b3070ec30d228cc4fe25403b43884d137c
guxiaona <15262526680@139.com>
2021-08-06 10:10:07 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+    <main-tab-bar></main-tab-bar>
+    <!-- 3.使用组件 -->
+  </div>
+</template>
+
+<script>
+// 1.先导入组件
+import MainTabBar from "./components/MainTabBar.vue";
+
+export default {
+  name: "App",
+  components: {
+    // 2.注册组件
+    MainTabBar,
+  },
+};
+</script>
+
+<style>
+@import "./assets/css/base.css";
+</style>
---
+++ src/components/MainTabBar.vue
@@ -0,0 +1,56 @@
+<template>
+  <tab-bar>
+    <tab-bar-item path="/home" activeColor="blue">
+      <img slot="item-icon" src="~assets/img/tabbar/home.png" alt="" />
+      <img
+        slot="item-icon-active"
+        src="~assets/img/tabbar/home_active.png"
+        alt=""
+      />
+      <div slot="item-text">首页</div>
+    </tab-bar-item>
+    <tab-bar-item path="/category" activeColor="blue">
+      <img slot="item-icon" src="~assets/img/tabbar/home.png" alt="" />
+      <img
+        slot="item-icon-active"
+        src="~assets/img/tabbar/home_active.png"
+        alt=""
+      />
+      <div slot="item-text">分类</div>
+    </tab-bar-item>
+    <tab-bar-item path="/cart" activeColor="blue">
+      <img slot="item-icon" src="~assets/img/tabbar/home.png" alt="" />
+      <img
+        slot="item-icon-active"
+        src="~assets/img/tabbar/home_active.png"
+        alt=""
+      />
+      <div slot="item-text">购物车</div>
+    </tab-bar-item>
+    <tab-bar-item path="/profile" activeColor="blue">
+      <img slot="item-icon" src="~assets/img/tabbar/home.png" alt="" />
+      <img
+        slot="item-icon-active"
+        src="~assets/img/tabbar/home_active.png"
+        alt=""
+      />
+      <div slot="item-text">我的</div>
+    </tab-bar-item>
+  </tab-bar>
+</template>
+
+<script>
+import TabBar from "components/tabbar/TabBar.vue";
+import TabBarItem from "components/tabbar/TabBarItem.vue";
+export default {
+  name: "MainTabBar",
+  components: {
+    // 2.注册组件
+    TabBar,
+    TabBarItem,
+  },
+};
+</script>
+
+<style>
+</style>
